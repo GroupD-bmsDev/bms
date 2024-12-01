4d03c2fb008a7d459d439bdf14a2112f89591585
tngidwlq <ddasik8352@gmail.com>
2024-12-01 20:22:36 +0900

    대시보드, 유지보수게시판 리스트 필터링 수정

--- src/main/resources/static/js/templates/admin/csf/maintenance.js
+++ src/main/resources/static/js/templates/admin/csf/maintenance.js
@@ -1,14 +1,55 @@
 /*
  * csf/maintenance.html 연동 스크립트 영역
  */
+let table;
+
+// 유지보수게시판-업무요청상태 class
+const taskStateClassMap = {
+    "접수": "state_receive",
+    "확인": "state_check",
+    "진행": "state_progress",
+    "완료": "state_complete",
+    "기타": "state_other",
+    "보류": "state_hold"
+};
+
+// 유지보수게시판-그룹디 담당부서별 class
+const gdDepartCodeClassMap = {
+    "미처리": "cat_no",
+    "마케팅 / 기획": "cat_mk",
+    "광고관리": "cat_ec",
+    "디자인": "cat_dg",
+    "퍼블리싱": "cat_pb",
+    "영상": "cat_bd",
+    "관리": "cat_cp",
+    "재무": "cat_fn"
+};
+
+
 $(document).ready(function () {
-    $('.main_board_state>span').click(function () {
+    
+    initDataTable(); // DataTable 초기화    
+    
+    $('.main_board_state>span').click(function () {        
         $('.main_board_state>span').removeClass('active');
         $(this).addClass('active');
+        
+        // 업무요청 클릭시 data-state 값으로 설정
+        var selectedState = $(this).data('state');
+        $('#taskStateType').val("selTaskState"); 
+        $('#taskStateWord').val(selectedState);                 
+        table.draw(); // 테이블 다시 그리기
     });
+
     $('.main_board_category>span').click(function () {
         $('.main_board_category>span').removeClass('active');
         $(this).addClass('active');
+        
+        // 부서 클릭한 span의 data-state 값으로 설정
+        var selectedState = $(this).data('state');
+        $('#gdDepartCodeType').val("selGdDepartCode");
+        $('#gdDepartCodeWord').val(selectedState); 
+        table.draw(); // 테이블 다시 그리기
     });
     $('.main_board_wrstate').click(function () {
         $(this).children('.state_select').slideToggle(100);
@@ -16,75 +57,7 @@ $(document).ready(function () {
     $('.main_board_wrcategory').click(function () {
         $(this).children('.category_select').slideToggle(100);
     });
-});
 
-
- // 업체 목록 테이블 생성
- function dataTableReload() {
-
-    var table = $('#employeeTable').DataTable({
-        "processing": true,
-        "serverSide": true,
-        "ajax": {
-            "url": "/admin/ems/employee_list",
-            "type": "POST",
-            "data": function(d) {
-                // 검색 조건을 데이터에 추가
-                d.fr_date = $('#fr_date').val();
-                d.to_date = $('#to_date').val();
-                d.searchType = $('#searchType').val();
-                d.search = $('#sch_word').val();
-            }
-        },
-        "columns": [
-            { "data": null, 
-                "className": "bm_wr_wdate",
-                "render": function(data, type, row) {
-                return formatDate(row.jobStartDate);
-            } },
-            { "data": "name" },
-            { "data": "departCode" },
-            { "data": "teamCode" },
-            { "data": null, 
-                "className": "bm_wr_wdate",
-                "render": function(data, type, row) {
-                return formatDate(row.birthday);
-                } 
-            },
-            { "data": "email" },
-            {
-                "data": "memo",
-                "defaultContent": ""  // 기본값 설정
-            },
-            { "data": "userid", "visible": false } // 히든 컬럼 추가
-        ],
-        "language": {
-            "paginate": {
-                "first": "처음",
-                "last": "맨끝",
-                "next": "다음",
-                "previous": "이전"
-            },
-            "info": "", // 정보 텍스트를 숨깁니다.
-            "lengthMenu": "", // 길이 메뉴 텍스트를 숨깁니다.
-            "zeroRecords": "데이터가 없습니다.", // 필요에 따라 조정
-            "infoEmpty": "" // 빈 정보 텍스트를 숨깁니다.
-        },
-        "dom": 'rtip', // 기본 페이징 네비게이션 컨트롤을 숨깁니다.
-        "createdRow": function(row, data, dataIndex) {
-            // 행에 userId 값을 data 속성으로 추가
-            $(row).attr('data-user-id', data.userid);
-        },
-        "initComplete": function(settings, json) {
-            updateCustomPagination(settings);
-        }
-    });
-
-    //화면 새로 그려질 때마다 해당 함수 호출 (페이지 이동, 검색 등)
-    table.on('draw', function () {
-        updateCustomPagination(table.settings());
-    });
-    
     //검색 버튼 클릭 이벤트
     $('#searchButton').on('click', function() {
         table.draw();
@@ -99,5 +72,112 @@ $(document).ready(function () {
             updateCustomPagination(table.settings());
         }
     });
+    
+    // 엔터 클릭 시, 검색 버튼 클릭 이벤트
+    $(document).on('keypress', function(event) {
+        if (event.which === 13) { 
+          $('#searchButton').click();
+          event.preventDefault(); 
+        }
+    });
+    
+    // 유지보수 요청 버튼 클릭 이벤트
+    $('body').on('click', 'tr', function() {
+        var seq = $(this).attr('seq');
+        showDetail(seq);
+    });
+});
 
+ // 유지보수게시판 목록 테이블 초기화
+function initDataTable() {
+    table = $('#maintenanceTable').DataTable({
+        "processing": true,
+        "serverSide": true,
+        "ajax": {
+            "url": "/admin/csf/maintenance_list",
+            "type": "POST",
+            "data": function(d) {
+                d.fr_date       = $('#fr_date').val();
+                d.to_date       = $('#to_date').val();
+                d.searchType    = $('#taskStateType').val();
+                d.search        = $('#taskStateWord').val();
+                d.searchType2   = $('#gdDepartCodeType').val();
+                d.search2       = $('#gdDepartCodeWord').val();
+                d.searchType3   = $('#searchType').val();
+                d.search3       = $('#sch_word').val();
+            }
+        },
+        "columns": [
+            { "data": null, "className": "mt_wr_wdate", "render": data => formatDate(data.regdate) }, // 업무 요청일
+            { "data": null, "className": "mt_wr_wdate", "render": data => formatDate(data.dueDate) }, // 완료 예정일
+            { "data": "siteName" }, // 작성자
+            { "data": "name" }, // 작성자
+            { "data": null, "className": "mt_wr_state", 
+                "render": data => {
+                    const taskStateClass = taskStateClassMap[data.strTaskState] || "state_receive";
+                    return `<span class="main_board_wrstate ${taskStateClass}" style="cursor: default">${data.strTaskState || '접수'}</span>`;
+                } 
+            }, // 상태
+            { "data": null, "className": "mt_wr_state",
+                "render": data => {
+                    const gdDepartCodeClass = gdDepartCodeClassMap[data.strGdDepartCode] || "cat_no";
+                    return `<span class="main_board_wrcategory ${gdDepartCodeClass}" style="cursor: default">${data.strGdDepartCode || '미처리'}</span>`;
+                } 
+            }, // 부서
+            { "data": "title" }, // 제목
+            { "data": "strGdUserKey" } // 담당자
+        ],
+        "language": {
+            "paginate": { "first": "처음", "last": "맨끝", "next": "다음", "previous": "이전" },
+            "zeroRecords": "데이터가 없습니다."
+        },
+        "dom": 'rtip',
+        "createdRow": function(row, data) {
+            $(row).attr('seq', data.seq);
+        },
+        "initComplete": function(settings) {
+            updateCustomPagination(settings);
+        }
+    });
+
+    // 테이블 이벤트 설정
+    table.on('draw', function () {
+        updateCustomPagination(table.settings());
+    });
+}
+
+function showDetail(seq) {
+    event.preventDefault();     
+
+    var form = $('<form>', {
+        method: 'POST',
+        action: '/admin/csf/maintenance_view'
+    });
+    form.append($('<input>', {
+        type: 'hidden',
+        name: 'seq',
+        value: seq
+    }));
+
+    $('body').append(form);
+    form.submit();
+}
+/**
+ * ajax  완료 후, 넘버링 데이타를 표시한다.
+ * @param {} settings 
+ */
+function updateCustomPagination(settings) {
+    var api = new $.fn.dataTable.Api(settings);
+    var pageInfo = api.page.info();
+    var paginationHtml = '';
+
+    paginationHtml += '<a href="#" data-page="0" class="pg_page pg_start' + (pageInfo.page === 0 ? ' disabled' : '') + '">처음</a>';
+    paginationHtml += '<a href="#" data-page="' + (pageInfo.page - 1) + '" class="pg_page pg_prev' + (pageInfo.page === 0 ? ' disabled' : '') + '">이전</a>';
+    for (var i = 0; i < pageInfo.recordsTotal; i++) {
+        paginationHtml += '<a href="#" data-page="' + i + '" class="pg_page' + (pageInfo.page === i ? ' pg_current' : '') + '">' + (i + 1) + '</a>';
+    }
+    paginationHtml += '<a href="#" data-page="' + (pageInfo.page + 1) + '" class="pg_page pg_next' + (pageInfo.page === pageInfo.recordsTotal - 1 ? ' disabled' : '') + '">다음</a>';
+    paginationHtml += '<a href="#" data-page="' + (pageInfo.pages - 1) + '" class="pg_page pg_end' + (pageInfo.page === pageInfo.recordsTotal - 1 ? ' disabled' : '') + '">맨끝</a>';
+
+    $('#customPagination').html(paginationHtml);
 }
